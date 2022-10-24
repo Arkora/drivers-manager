1dde36542e6d8150eb5962597ce4b64631cdec9f
Arko Rapollari <rapolari98@hotmail.com>
2022-10-24 20:50:03 +0300

    delete some functions on userController

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import { driversReports, getDriverReports } from '../controllers/aggregations.js'
-import { register,login, getUser,userReports, getUserWithSum } from '../controllers/userController.js'
+import { register,login } from '../controllers/userController.js'
 
 const router = express.Router()
 
-// router.get('/report',userReports)
+
 router.get('/report',driversReports)
 router.get('/:id',getDriverReports)
-//router.get('/sums/:id',getUserWithSum)
 router.post('/login',login)
 router.post('/register',register)
 
